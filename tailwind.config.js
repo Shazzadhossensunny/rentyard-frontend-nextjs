8e26212e4bf5a9a552eaaa120551524b3e0ecf27
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2025-06-28 20:48:41 +0600

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#316EED",
+          foreground: "#FFFFFF",
+        },
+        stock: {
+          DEFAULT: "#E0E0E0",
+        },
+        text: {
+          primary: "#272B35",
+          secondary: "#6F6C6A",
+        },
+        error: "#FF6A62",
+        background: {
+          card: "#F4F4F4",
+          selected: "#F9FBFF",
+          upload: "#F6F9FF",
+        },
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
